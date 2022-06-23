14a9f18f1751be19a6cf8ab9f45b5ba4906a26bf
Arancreat <30105608+Arancreat@users.noreply.github.com>
2022-06-23 22:25:33 +0900

    Backend project init

---
+++ traineemanager/src/main/java/ru/albank/trainee/traineemanager/TraineemanagerApplication.java
@@ -0,0 +1,13 @@
+package ru.albank.trainee.traineemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineemanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraineemanagerApplication.class, args);
+	}
+
+}
---
+++ traineemanager/src/test/java/ru/albank/trainee/traineemanager/TraineemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.albank.trainee.traineemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
