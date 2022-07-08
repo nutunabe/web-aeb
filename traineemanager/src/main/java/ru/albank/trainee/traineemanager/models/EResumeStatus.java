0a08b890998f1d09defd878f82ea56c116cbde04
Witawe <texys@list.ru>
2022-07-08 13:12:42 +0900

    modified enum resume status

--- traineemanager/src/main/java/ru/albank/trainee/traineemanager/models/EResumeStatus.java
+++ traineemanager/src/main/java/ru/albank/trainee/traineemanager/models/EResumeStatus.java
@@ -3,5 +3,9 @@ package ru.albank.trainee.traineemanager.models;
 public enum EResumeStatus {
     STATUS_NOT_CHECKED,
 
-    STATUS_CHECKED
+    STATUS_CHECKED,
+
+    STATUS_APPROVED,
+
+    STATUS_EJECTED
 }
